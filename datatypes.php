dde2d68ea1508d571c66cd5dca14d333fccef501
shyam8218 <shyampandit8218@gmail.com>
2021-03-13 11:38:58 +0530

    ff

---
+++ datatypes.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>data types in  php file</title>
+</head>
+<body>
+     <?php
+         $name ="121"; 
+        echo $name."<br>";
+        
+
+        var_dump($name);
+     ?>
+</body>
+</html>
